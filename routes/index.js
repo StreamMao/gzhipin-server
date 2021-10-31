0a5c00d021a97bf01f6119b8bac2de54693ce5c4
StreamMao <maosijun1995@gmail.com>
2021-10-31 17:16:58 -0400

    server router and API test, using POSTMAN

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/register', function(req, res) {
+  const {username, password} = req.body
+  if (username === 'admin') {//注册失败
+    //返回相应数据：失败
+    res.send({code: 1, msg:'此用户已存在'})
+  } else { //注册成功
+    //返回相应数据： 成功
+    res.send({code: 0, data:{id: 'abc123', username, password}})
+  }
+})
+
 module.exports = router;
